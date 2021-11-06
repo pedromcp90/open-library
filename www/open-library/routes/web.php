e5249b466a05fc3673c7ed957ece68abed94a9ba
Pedro Cruz <pedromcp90@gmail.com>
2021-11-06 16:37:44 +0000

    Added login routes

--- www/open-library/routes/web.php
+++ www/open-library/routes/web.php
@@ -23,3 +23,7 @@ Route::get('/', function () {
 
 //Add Book Routes
 Route::resource('book', BookController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
