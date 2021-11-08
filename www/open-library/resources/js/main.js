21c430323ce4d1b50e1db8a64329de72939848d1
Pedro Cruz <pedromcp90@gmail.com>
2021-11-08 23:00:52 +0000

    Added ajax call to create authors and categories

--- www/open-library/resources/js/main.js
+++ www/open-library/resources/js/main.js
@@ -13,19 +13,43 @@
         $('.categories-select').on('chosen:no_results', function () {
             attachNewEntryButton('category');
         });
-        $("body").on('click', '.new-entry-button', function(){
+        $("body").on('click', '.new-entry-button', function () {
             const formName = $(this).data('table');
             $("#newEntryModal .modal-title").html('Create ' + formName);
         });
-        $(".new-entry-form-wrapper form").on('submit', function(ev){
+        $(".new-entry-form-wrapper form").on('submit', function (ev) {
             ev.preventDefault();
-            alert("AAA");
+            //make an ajax request to insert the new entry
+            const url = $(this).prop('action');
+            let table = _.last(url.split('/'));
+            table = Pluralize(table);
+            let ajaxObject = {
+                type: 'POST',
+                data: $(this).serialize(),
+                dataType: 'json',
+                url: '/api/' + table,
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (jqXHR, textStatus) {
+
+                }
+            };
+
+            $.ajax(ajaxObject);
         })
     });
+
+    /**
+     * Hide the forms when modal is dismissed.
+     */
+    $('#newEntryModal').on('hidden.bs.modal', function (e) {
+        $(".new-entry-form-wrapper").hide();
+    })
 
     function attachNewEntryButton(table) {
         let btn = `<div class="new-entry-wrapper"><button data-toggle="modal" data-target="#newEntryModal" class="new-entry-button btn btn-success btn-sm" type="button" data-table="${table}"}>Add ${table}</button></div>`
         $(".no-results").append(btn);
-        $("#"+table+"-form").show();
+        $("#" + table + "-form").show();
     }
 })(window, document, jQuery)
