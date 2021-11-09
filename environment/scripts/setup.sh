076319426f8a7e0893b1358406803fb71dca27ca
Pedro Cruz <pedromcp90@gmail.com>
2021-11-09 02:59:29 +0000

    Added composer container to the docker composer

---
+++ environment/scripts/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+printf "\n## INSTALANDO UTILES ##\n"
+apt-get -y update
+apt-get -y install wget curl
+#-----------------------------------------------------------------
+
+printf "\n## INSTALANDO DOCKER ##\n"
+    apt-get -yq install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get -y install docker-ce
+
+#------------------rootless mode----------------------------------
+
+sudo usermod -aG docker $USER
+
+#-----------------------------------------------------------------
+
+printf "\n## INSTLANDO DOCKER COMPOSE ##\n"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+#-----------------------------------------------------------------
+#
+
+
+chmod -R 777 ../logs
+
+printf "\n## AÑADIENDO RESOLUCIÓN DE NOMBRES .LOC ##\n"
+echo "172.20.1.1   openlibrary.loc" >> /etc/hosts
+echo "172.20.1.1   www.openlibrary.loc" >> /etc/hosts
+#-----------------------------------------------------------------
+
+
+printf "\n## LEVANTANDO CONTENEDORES ##\n"
+cd .. && docker-compose up -d
+docker-compose ps
+#-----------------------------------------------------------------
+
+
+
+echo "FIN [Enter]"
+read
+#reboot
