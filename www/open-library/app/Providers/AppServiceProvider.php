cde0c65c4415ce75e66b164123020b0ebfa68c01
Pedro Cruz <pedromcp90@gmail.com>
2021-11-10 21:00:37 +0000

    Added new breadcrumb component

--- www/open-library/app/Providers/AppServiceProvider.php
+++ www/open-library/app/Providers/AppServiceProvider.php
@@ -2,13 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models;
-use App\Models\Author;
-use App\Models\Category;
-use App\Models\Navbar;
-//use Illuminate\View\View;
+use App\Helpers\Helper;
 use View;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,41 +26,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //Add pagination
-        Paginator::useBootstrap();
+        //Paginator::useBootstrap();
 
         //Add the navigation elements
         View::composer('*', function ($view) {
-            //$navbars = Navbar::orderBy('ordering')->get();
-            //$view->with('navbars', $navbars);
-
-            $controllers = [
-                'book' => 'Book',
-                'author' => 'Author',
-                'category' => 'Category',
-                'publisher' => 'Publisher',
-            ];
-            $nav = [];
-            $methods = [
-                '' => 'List',
-                'create' => 'Create'
-            ];
-            foreach ($controllers as $controllerPath => $controllerName) {
-                $children = [];
-                foreach ($methods as $methodPath => $methodName) {
-                    $children[] = [
-                        'text' => $methodName,
-                        'url' => '/'.$controllerPath . '/' . $methodPath
-                    ];
-                }
-
-                $nav[] = [
-                    'text' => $controllerName,
-                    'url' => $controllerPath,
-                    'children' => $children
-                ];
-            }
-
+            $nav = Helper::generateNav();
             $view->with('navbars', $nav);
+            $pathInfo = Helper::getPathInfoAsArray();
+            $view->with('currentPath', $pathInfo);
         });
     }
 }
