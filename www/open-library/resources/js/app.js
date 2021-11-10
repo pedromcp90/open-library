a44ecc528eab8a17dcf79d86d26c62b1b0cf7ffd
Pedro Cruz <pedromcp90@gmail.com>
2021-11-10 20:56:09 +0000

    -Refactor main.js -Added DataTables , bootstrap, tooltip and Datepicker

--- www/open-library/resources/js/app.js
+++ www/open-library/resources/js/app.js
@@ -4,14 +4,15 @@ import Cookies from '../themes/argon/vendor/js-cookie/js.cookie.js'
 window.Cookies = Cookies;
 import Pluralize from '../vendor/pluralize';
 window.Pluralize = Pluralize;
+import DataTable from '../themes/argon/vendor/datatables.net/js/jquery.dataTables.min.js';
+window.DataTable = DataTable;
 
+require('../themes/argon/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js')
+require('../themes/argon/vendor/datatables.net-select/js/dataTables.select.min.js')
+require('../themes/argon/vendor/datatables.net-select-bs4/js/select.bootstrap4.min.js')
+require('../themes/argon/vendor/datatables.net-buttons/js/dataTables.buttons.min.js')
+require('../themes/argon/vendor/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')
+require('../themes/argon/vendor/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')
+require('../themes/argon/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')
 require('./bootstrap');
-//require('../themes/argon/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')
-//require('../themes/argon/vendor/js-cookie/js.cookie.js')
-require('../themes/argon/vendor/jquery.scrollbar/jquery.scrollbar.min.js')
-require('../themes/argon/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js')
-require('../themes/argon/vendor/select2/dist/js/select2.min.js')
-//require('../themes/argon/vendor/chart.js/dist/Chart.min.js')
-//require('../themes/argon/vendor/chart.js/dist/Chart.extension.js')
-require('../themes/argon/js/argon.js')
 require('./main')
--- www/open-library/resources/js/main.js
+++ www/open-library/resources/js/main.js
@@ -1,50 +1,50 @@
 "use strict";
+
+const { initial } = require("lodash");
+
 (function (window, document, $) {
     $().ready(function () {
+        initialize();
+        attachEvents();
+    });
+
+
+    function initialize() {
+        /*
+        * Initialize tooltip
+        */
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        });
+
+        /**
+         * Init datatables
+         */
+        $('table').DataTable({
+            "language": {
+                "paginate": {
+                    "previous": "<",
+                    "next": ">"
+                }
+            }
+        });
+        $('.dataTables_length').addClass('bs-select');
+
+        /**
+         * initializa datepicker
+         */
+        $('#yearDatepicker').datepicker({
+            format: "yyyy",
+            viewMode: "years",
+            minViewMode: "years",
+            autoclose: true //to close picker once year is selected
+        });
+
         /*
         Initialize chosen library
         */
         $('.chosen-select').chosen();
-
-        $('.authors-select').on('chosen:no_results', function (ev, params) {
-            attachNewEntryButton('author', params.chosen);
-        });
-        $('.categories-select').on('chosen:no_results', function (ev, params) {
-            attachNewEntryButton('category', params.chosen);
-        });
-        $("body").on('click', '.new-entry-button', function () {
-            const formName = $(this).data('entity_name');
-            $("#newEntryModal .modal-title").html('Create ' + formName);
-        });
-        $(".new-entry-form-wrapper form").on('submit', function (ev) {
-            ev.preventDefault();
-            //make an ajax request to insert the new entry
-            const url = $(this).prop('action');
-            let entityName = _.last(url.split('/'));
-            entityName = Pluralize(entityName);
-            let ajaxObject = {
-                type: 'POST',
-                data: $(this).serialize(),
-                dataType: 'json',
-                url: '/api/' + entityName,
-                success: function (response) {
-                    createNewOption(response.data, entityName);
-                },
-                error: function (jqXHR, textStatus) {
-
-                }
-            };
-
-            $.ajax(ajaxObject);
-        })
-    });
-
-    /**
-     * Hide the forms when modal is dismissed.
-     */
-    $('#newEntryModal').on('hidden.bs.modal', function (e) {
-        $(".new-entry-form-wrapper").hide();
-    })
+    }
 
     function attachNewEntryButton(entityName, chosenParams) {
         /**
@@ -56,22 +56,84 @@
         let newEntryButton = `<div class="new-entry-wrapper"><button data-toggle="modal" data-target="#newEntryModal" class="new-entry-button btn btn-success btn-sm" type="button" data-entity-name="${entityName}"} data-input-value="${inputValue}"}>Add ${entityName}</button></div>`
         $(".no-results").append(newEntryButton);
 
-        chosen-search-input
         /**
          * Set the input value
          */
         let inputFields = $("#" + entityName + "-form").find("input[type='text']");
         $(inputFields[0]).val(inputValue);
 
-
         $("#" + entityName + "-form").show();
     }
 
-    function createNewOption(optionData, entityName) {
-        let value = '';
+    function attachEvents() {
+        $('.authors-select').on('chosen:no_results', function (ev, params) {
+            attachNewEntryButton('author', params.chosen);
+        });
+
+        $('.categories-select').on('chosen:no_results', function (ev, params) {
+            attachNewEntryButton('category', params.chosen);
+        });
+
+        $("body").on('click', '.new-entry-button', function () {
+            const formName = $(this).data('entity_name');
+            $("#newEntryModal .modal-title").html('Create ' + formName);
+        });
+
+        $(".new-entry-form-wrapper form").on('submit', newEntryFromSubmit);
+
+        $(".btn-form-submit").on('click', function (ev) {
+            if (window.confirm("Are you sure of deleting this element?")) {
+                $(this).parents("form").submit();
+            }
+        });
+
         /**
-         * Set the option value depending on its type.
+         * Hide the forms when modal is dismissed.
          */
+        $('#newEntryModal').on('hidden.bs.modal', function (e) {
+            $(".new-entry-form-wrapper").hide();
+        })
+    }
+
+    function newEntryFromSubmit(ev) {
+        const that = this;
+        //Prevent the form to be submitted normally
+        ev.preventDefault();
+
+        //Get the form action to perform the same one over the rest API
+        const url = $(this).prop('action');
+
+        //Get the entity name
+        let entityName = _.last(url.split('/'));
+        entityName = Pluralize(entityName);
+
+        //Serialize the form data
+        let serializedFormData = $(this).serialize();
+
+        //Generate ajax call object
+        let ajaxObject = {
+            type: 'POST',
+            data: serializedFormData,
+            dataType: 'json',
+            url: '/api/' + entityName,
+            success: function (response) {
+                createNewSelectOption(response, entityName);
+            },
+            error: ajaxError
+        };
+        //Send the request
+        $.ajax(ajaxObject);
+    }
+
+    /**
+     * Generates the returned select option that has been created
+     * @param {*} response
+     */
+    function createNewSelectOption(response, entityName) {
+        const optionData = response.data;
+        let value = '';
+
+        //Set the option value depending on its type.
         switch (entityName) {
             case 'authors':
                 value = optionData.first_name + ' ' + optionData.last_name;
@@ -81,18 +143,19 @@
                 break;
         }
 
-        /**
-         * Create the new option, append to the select and trigger the update method
-         */
+        //Create the new option, append to the select and trigger the update method
         let option = `<option value="${optionData.id}" selected >${value}</option>`
         $("." + entityName + "-select").append(option).trigger('chosen:updated');
-        /**
-         * Hide the modal
-         */
+
+        //Hide the modal
         $("#newEntryModal").modal('hide');
-        /**
-         * Finally reset the form
-         */
+
+        //Finally reset the form
         $(".new-entry-form-wrapper form").trigger("reset");
+    }
+
+    function ajaxError(jqXHR, textStatus) {
+        alert("Error inserting new element");
+        console.error('Error' + textStatus);
     }
 })(window, document, jQuery)
