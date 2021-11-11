4b22b4a354bc6f5893a43f2e4141ef40033222d2
Pedro Cruz <pedromcp90@gmail.com>
2021-11-11 17:09:02 +0000

    Added Users Seeder Added UsersTableSeeder

--- www/open-library/database/seeders/UserSeeder.php
+++ www/open-library/database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
---
+++ www/open-library/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userRole = config('roles.models.role')::where('name', '=', 'User')->first();
+        $adminRole = config('roles.models.role')::where('name', '=', 'Admin')->first();
+        $permissions = config('roles.models.permission')::all();
+
+        /*
+         * Add Users
+         *
+         */
+        if (config('roles.models.defaultUser')::where('email', '=', 'admin@admin.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'first_name'     => 'Admin',
+                'lastt_name'     => 'Admin',
+                'email'    => 'admin@admin.com',
+                'password' => bcrypt('password'),
+            ]);
+
+            $newUser->attachRole($adminRole);
+            foreach ($permissions as $permission) {
+                $newUser->attachPermission($permission);
+            }
+        }
+
+        if (config('roles.models.defaultUser')::where('email', '=', 'user@user.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'first_name'     => 'User',
+                'last_name'     => 'Last',
+                'email'    => 'user@user.com',
+                'password' => bcrypt('password'),
+            ]);
+
+            $newUser->attachRole($userRole);
+        }
+    }
+}
