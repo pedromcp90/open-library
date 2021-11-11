f1b8fc129553cb75130c7e10523050200eae119c
Pedro Cruz <pedromcp90@gmail.com>
2021-11-11 17:30:14 +0000

    Added header in home page

--- www/open-library/resources/views/book/publicIndex.blade.php
+++ www/open-library/resources/views/book/publicIndex.blade.php
@@ -2,6 +2,18 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-12">
+                <div class="jumbotron">
+                    <h1 class="display-4">Welcome to Open Library</h1>
+                    <p class="lead">This website is a Library CRUD example that allows you to manage a library catalog.</p>
+                    <hr class="my-4">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
             @foreach ($books as $book)
                 <div class="col-sm-4">
                     <div class="card">
