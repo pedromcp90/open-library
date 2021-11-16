47a5a8c478a8f9c5adddbb3df8c16e1760d34326
Pedro Cruz <pedromcp90@gmail.com>
2021-11-16 21:24:07 +0000

    Included some dependencies that needed to be included from node_modules

--- www/open-library/resources/js/app.js
+++ www/open-library/resources/js/app.js
@@ -4,15 +4,14 @@ import Cookies from '../themes/argon/vendor/js-cookie/js.cookie.js'
 window.Cookies = Cookies;
 import Pluralize from '../vendor/pluralize';
 window.Pluralize = Pluralize;
-import DataTable from '../themes/argon/vendor/datatables.net/js/jquery.dataTables.min.js';
-window.DataTable = DataTable;
+window.DataTable = require( 'datatables.net' );
 
-require('../themes/argon/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js')
-require('../themes/argon/vendor/datatables.net-select/js/dataTables.select.min.js')
-require('../themes/argon/vendor/datatables.net-select-bs4/js/select.bootstrap4.min.js')
-require('../themes/argon/vendor/datatables.net-buttons/js/dataTables.buttons.min.js')
-require('../themes/argon/vendor/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')
-require('../themes/argon/vendor/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')
+require('datatables.net-select')
+require('datatables.net-select-bs4')
+require('datatables.net-buttons')
+require('datatables.net-buttons-bs4')
+require('datatables.net-responsive')
+require('datatables.net-responsive-bs4')
 require('../themes/argon/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')
 require('./bootstrap');
 require('./main')
