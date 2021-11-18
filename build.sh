429d67808f05734b96d50b7f7e2b8727c42f786e
Pedro Cruz <pedromcp90@gmail.com>
2021-11-18 00:07:34 +0000

    Added laravel storage link and storage permissions to build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cd environment && docker-compose up -d && docker-compose exec www php open-library/artisan migrate:fresh --seed
+echo "|-----------BUILDING DOCKER CONTAINERS-----------|"
+cd environment && docker-compose up -d
+echo "|-----------CREATING LARAVEL STORAGE LINK-----------|"
+docker-compose exec www php open-library/artisan storage:link
+echo "|-----------EXECUTE DB MIGRATIONS-----------|"
+docker-compose exec www php open-library/artisan migrate:fresh --seed
+echo "|-----------ADD PERMISSIONS FOR STORAGE DIRECTORY-----------|"
+chmod -R 777 ../www/open-library/storage
